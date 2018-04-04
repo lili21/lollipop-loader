faa0b78bfb8537747de4f996a30fc4b944fd688f
li.li <mark.li210812@gmail.com>
2018-04-04 11:26:05 +0800

    :tada: Hello, lollipop-loader

---
+++ README.md
@@ -0,0 +1,16 @@
+## lollipop-loader
+> React Loading Component
+
+### Installation
+
+```bash
+$ yarn add lollipop-loader
+```
+
+### Usage
+
+```javascript
+import Loading from 'lollipop-loader'
+
+ReactDOM.render(<Loading />, container)
+```
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Svg = styled.svg`
+  stroke-linecap: round;
+  animation: lollipop-circle 1.5s ease infinite, lollipop-rotate 1.5s linear infinte;
+  @keyframes lollipop-rotate {
+    0% {
+      transform: rotate(0)
+    }
+
+    100% {
+      transform: rotate(360deg)
+    }
+  }
+  ${lollipopCircle}  
+`
+
+function lollipopCircle ({ size, dash }) {
+  const circumference = 3.14 * (size - dash)
+  return `
+    @keyframes lollipop-circle {
+      0% {
+        stroke-dasharray: ${dash}, ${circumference};
+        stroke-dashoffset: 0
+      }
+
+      50% {
+        stroke-dasharray: ${circumference}, ${circumference};
+        stroke-dashoffset: -${circumference * 0.25};
+      }
+
+      100% {
+        stroke-dasharray: ${circumference}, ${circumference};
+        stroke-dashoffset: ${dash - circumference}
+      }
+    }
+  `
+}
+
+export default function Loading ({ size, dash, color }) {
+  const radius = (size - dash) / 2
+  return (
+    <div>
+      <Svg width={size} height={size} size={size} dash={dash}>
+        <circle cx={size / 2} cy={size / 2} r={radius} fill="none" stroke={color} stroke-width={dash} />
+      </Svg>
+    </div>
+  )
+}
+
+Loading.propTypes = {
+  size: PropTypes.number,
+  dash: PropTypes.number,
+  color: PropTypes.string
+}
+
+Loading.defaultProps = {
+  size: 36,
+  dash: 4,
+  color: 'mediumseagreen'
+}
